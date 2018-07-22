72d10df6b3777014070ca91a0bde4732481ad2af
Vaidotas Gaulia <blangas2000@yahoo.com>
2018-07-22 10:04:06 +0200

    feat: search works

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Bookshelf from './Bookshelf.js'
+import Book from './Book.js'
+import SearchList from './SearchList.js'
 
 class BooksApp extends React.Component {
   state = {
     books: [],
     showSearchPage: false,
-    query: ''
+    query: '',
+    searchQuery: []
   }
 
   getBooks = () => {
@@ -22,33 +25,60 @@ class BooksApp extends React.Component {
     this.getBooks()
   }
 
-  // clearQuery =
+  searchQuery = (query) => {
+    if (query) {
+      BooksAPI.search(query.trim())
+      .then(response => {
+        console.log(response)
+        this.setState({ searchQuery: response })
+      })
+    } else {
+      this.setState({ searchQuery: []})
+    }
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
 
   render() {
     return (
       <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                <input
+                  type="text"
+                  placeholder="Search by title or author"
+                  onChange={(e) => this.searchQuery(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {this.state.searchQuery
+                  .map(book => (
+                    <Book
+                      book={book}
+                      key={book.id}
+                      updateBook={this.getBooks}
+                    />
+                  ))}
+              </ol>
+            </div>
+          </div>
+        ) : (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
-            {this.state.showSearchPage && (
-              <div className="search-books">
-                <div className="search-books-bar">
-                  <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-                  <div className="search-books-input-wrapper">
-                    <input type="text" placeholder="Search by title or author"/>
-                  </div>
-                </div>
-                <div className="search-books-results">
-                  <ol className="books-grid"></ol>
-                </div>
-              </div>
-            )}
             <Bookshelf
               id="currentlyReading"
               bookshelf="Currently Reading"
               books={this.state.books}
-              updateBooks={this.getBooks}
+              updateBooks={this.getBooks.bind(this)}
             />
             <Bookshelf
               id="wantToRead"
@@ -62,12 +92,11 @@ class BooksApp extends React.Component {
               books={this.state.books}
               updateBooks={this.getBooks.bind(this)}
             />
-            {!this.state.showSearchPage && (
-              <div className="open-search">
-                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-              </div>
-            )}
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
           </div>
+        )}
       </div>
     )
   }
---
+++ src/SearchList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
+
+class SearchList extends Component {
+  render() {
+    return (
+      <li>
+
+      </li>
+    )
+  }
+}
+
+export default SearchList
